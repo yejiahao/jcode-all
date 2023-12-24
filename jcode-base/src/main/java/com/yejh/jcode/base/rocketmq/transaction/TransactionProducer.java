a1028fe79b40c01e6896f801182ba1d14d36b81a
yejiahao <yejh.1248@qq.com>
2023-12-24 18:15:55 +0800

    :arrow_up: 升级 RocketMQ 依赖

--- jcode-base/src/main/java/com/yejh/jcode/base/rocketmq/transaction/TransactionProducer.java
+++ jcode-base/src/main/java/com/yejh/jcode/base/rocketmq/transaction/TransactionProducer.java
@@ -1,14 +1,19 @@
 package com.yejh.jcode.base.rocketmq.transaction;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.LocalTransactionState;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.TransactionListener;
 import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Objects;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * It can be thought of as a two-phase commit message implementation to ensure eventual consistency in distributed system.
@@ -18,43 +23,69 @@ import java.util.concurrent.*;
  * @create 2019-11-18
  * @since 4.6.0
  */
+@Slf4j
 public class TransactionProducer {
 
     public static void main(String[] args) throws MQClientException, InterruptedException {
-        TransactionListener transactionListener = new TransactionListenerImpl();
-        TransactionMQProducer producer = new TransactionMQProducer("please_rename_unique_group_name");
+        TransactionMQProducer producer = new TransactionMQProducer("trans_producer_group_001");
         ExecutorService executorService = new ThreadPoolExecutor(
                 2,
                 5,
                 100,
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(2000),
-                r -> {
-                    Thread thread = new Thread(r);
-                    thread.setName("client-transaction-msg-check-thread");
-                    return thread;
-                });
+                r -> new Thread(r, "client-transaction-msg-check-thread"));
 
-
+        producer.setNamesrvAddr("vm5.yejh.cn:9876");
         producer.setExecutorService(executorService);
-        producer.setTransactionListener(transactionListener);
+        producer.setTransactionListener(new TransactionListenerImpl());
         producer.start();
 
-        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        String[] tags = {"TagA", "TagB", "TagC", "TagD", "TagE"};
         for (int i = 0; i < 10; i++) {
             try {
-                Message msg = new Message("TopicTest1234", tags[i % tags.length], "KEY" + i,
-                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                Message msg = new Message("TopicTest1234", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
                 SendResult sendResult = producer.sendMessageInTransaction(msg, null);
-                System.out.printf("%s%n", sendResult);
+                log.info("{}", sendResult);
             } catch (MQClientException | UnsupportedEncodingException e) {
-                e.printStackTrace();
+                log.error("exception: {}", e.getMessage(), e);
             }
         }
 
         for (int i = 0; i < 100_000; i++) {
-            Thread.sleep(1000L);
+            TimeUnit.MILLISECONDS.sleep(1000L);
         }
         producer.shutdown();
+    }
+}
+
+class TransactionListenerImpl implements TransactionListener {
+
+    private AtomicInteger transactionIndex = new AtomicInteger(0);
+
+    private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
+
+    @Override
+    public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+        int value = transactionIndex.getAndIncrement();
+        int status = value % 3;
+        localTrans.put(msg.getTransactionId(), status);
+        return LocalTransactionState.UNKNOW;
+    }
+
+    @Override
+    public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+        Integer status = localTrans.get(msg.getTransactionId());
+        if (Objects.nonNull(status)) {
+            switch (status) {
+                case 0:
+                    return LocalTransactionState.UNKNOW;
+                case 1:
+                    return LocalTransactionState.COMMIT_MESSAGE;
+                case 2:
+                    return LocalTransactionState.ROLLBACK_MESSAGE;
+            }
+        }
+        return LocalTransactionState.COMMIT_MESSAGE;
     }
 }
